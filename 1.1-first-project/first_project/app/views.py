67384d1caa6fd1c0a2d48acc9ae4ba3051c61d4b
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-11-22 11:37:32 +0300

    Дописание функций home_view, time_view, workdir_view(несколько вариантов)

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+import datetime
+import os
 
 
 def home_view(request):
@@ -8,28 +10,36 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir'),
     }
-    
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
     context = {
         'pages': pages
     }
-    return render(request, template_name, context)
+    return render(request, template_name, context=context)
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
+    # обратите внимание – здесь HTML шаблона нет,
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.datetime.now()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
-    raise NotImplemented
+    # разобрал несколько вариантов
+    # первый с использованием templates
+    # второй (внизу) вывод списком
+    example_dir = '/Users/Admin/PycharmProjects/Django_Project'
+    content = os.listdir(example_dir)
+    return render(request, 'app/all_files_of_directory.html', context={'content': content})
+
+# def workdir_view(request):
+#     # по аналогии с `time_view`, напишите код,
+#     # который возвращает список файлов в рабочей директории
+#     example_dir = '/Users/Admin/PycharmProjects/Django_Project'
+#     all_files = f'Рабочая директория содержит файлы: {os.listdir(example_dir)}'
+#     return HttpResponse(all_files)
