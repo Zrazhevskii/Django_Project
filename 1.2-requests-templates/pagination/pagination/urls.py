6cecb07ec44e8baa12bdba322caf45d0872e831d
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-11-26 18:10:17 +0300

    изменение путей, добавление urlpatterns2

--- 1.2-requests-templates/pagination/pagination/urls.py
+++ 1.2-requests-templates/pagination/pagination/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 
 from django.urls import path, include
+from stations.views import index, bus_stations
+
+urlpatterns2 = [
+    path('', index, name='index'),
+    path('bus_stations/', bus_stations, name='bus_stations'),
+]
 
 urlpatterns = [
-    path('', include('stations.urls')),
+    path('', include(urlpatterns2)),
 ]
