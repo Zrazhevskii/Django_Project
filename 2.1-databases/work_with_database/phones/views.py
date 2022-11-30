3af6005417f29dbd21734fc91318ecb8db15b6b3
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-11-30 17:38:41 +0300

    изменение написание show_catalog

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -6,9 +7,20 @@ def index(request):
 
 
 def show_catalog(request):
-    template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
+    templates = 'catalog.html'
+    phones = Phone.objects.all()
+    sort = request.GET.get('sort', '')
+
+    if sort == 'name':
+        phones_str = phones.order_by('name')
+    elif sort == 'max_price':
+        phones_str = phones.order_by('max_price')
+    elif sort == 'mix_price':
+        phones_str = phones.order_by('mix_price').reverse()
+    else:
+        phones
+    context = {'phones': phones_str}
+    return render(request, templates, context)
 
 
 def show_product(request, slug):
