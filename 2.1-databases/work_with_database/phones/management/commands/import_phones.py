d868b99899e8a3e0bad0484fac256e9280d15b34
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-12-03 15:28:39 +0300

    написание handle

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,6 +2,7 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from django.template.defaultfilters import slugify
 
 
 class Command(BaseCommand):
@@ -9,9 +10,16 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
-            phones = list(csv.DictReader(file, delimiter=';'))
+        with open('phones1.csv', encoding='utf-8') as file:
+            phons = csv.DictReader(file, delimiter=';')
 
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            for phone in phons:
+                Phone.objects.create(
+                    id=phone['id'],
+                    name=phone['name'],
+                    image=phone['image'],
+                    price=int(phone['price']),
+                    release_date=phone['release_date'],
+                    lte_exists=phone['lte_exists'],
+                    slug=slugify(phone['name'])
+                )
