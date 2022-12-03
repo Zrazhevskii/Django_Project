c02e7d58b91e82fd55501d05584a949f603db2d0
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-12-03 15:20:30 +0300

    наполнение class Phone

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -3,4 +3,13 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200, null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.URLField(default=None)
+    release_date = models.DateField(default=None)
+    lte_exists = models.BooleanField(default=None)
+    slug = models.SlugField(max_length=200)
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
