e265f5865be753fbc49cb1669ea0ce9a6991bc43
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-12-07 21:25:34 +0300

    реализация свойства inlines

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, ArticleTag, Tag
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+
+    def clean(self):
+        checked = 0
+        for form in self.forms:
+
+            if form.cleaned_data.get('is_main') is True:
+                checked += 1
+
+        if checked > 1:
+            raise ValidationError('Основным может быть только один раздел')
+
+        elif checked == 0:
+            raise ValidationError('Укажите основной раздел')
+
+        return super().clean()
+
+
+class RelationshipInline(admin.TabularInline):
+    model = ArticleTag
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
+
+
+@admin.register(Tag)
+class ScopesAdmin(admin.ModelAdmin):
     pass
