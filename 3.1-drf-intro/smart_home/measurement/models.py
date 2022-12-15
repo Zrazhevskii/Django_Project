87aba00517bf54e5847ee75c5cc6649cb47ea953
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-12-15 14:15:31 +0300

    изменение названия поля в Measurement

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -14,7 +14,7 @@ class Sensor(models.Model):
 class Measurement(models.Model):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
     measuring_temp = models.DecimalField(max_digits=5, decimal_places=2)
-    date_of_measurement = models.DateTimeField(auto_now=True)
+    time_of_measurement = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
