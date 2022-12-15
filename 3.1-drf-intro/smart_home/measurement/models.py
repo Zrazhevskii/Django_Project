2b8ef83831dc9d109b8451bd72033b1cf1a88f6a
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-12-15 14:02:53 +0300

    создание моделей Sensor, Measurement

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=80)
+    description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    measuring_temp = models.DecimalField(max_digits=5, decimal_places=2)
+    date_of_measurement = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
