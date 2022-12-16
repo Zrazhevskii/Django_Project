64f061768383e1da4752b63287d9e6ae2237ad6e
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-12-16 14:40:43 +0300

    регистрация admin

--- 3.1-drf-intro/smart_home/measurement/admin.py
+++ 3.1-drf-intro/smart_home/measurement/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from measurement.models import Sensor, Measurement
+
+
+admin.site.register(Sensor)
+
+admin.site.register(Measurement)
+
