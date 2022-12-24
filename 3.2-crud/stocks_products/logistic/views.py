276edd6651588349d17693cb61577641f50f29d5
Zrazhevskii <vbn-134@yandex.ru>
2022-12-24 16:05:33 +0300

    добавление параметров фильтрации

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,16 +1,30 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from django.http import HttpResponse
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
 
+def home(request):
+    return HttpResponse('<h1> Нет большей неудачи, чем перестать пытаться </h1>')
+
+
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title', 'description']
 
 
-class StockViewSet(ModelViewSet):
+class StockViewSet(viewsets.ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['address', 'products', 'positions']
+
+    # search_fields = ['products__title', 'positions__products__title']
