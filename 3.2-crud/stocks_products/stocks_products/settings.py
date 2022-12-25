d8ef5fc66a4356eaac52179071573db71c9a073a
Zrazhevskii <vbn-134@yandex.ru>
2022-12-25 18:11:56 +0300

    настройка пагинатора

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -133,5 +133,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTERS_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1,
 }
