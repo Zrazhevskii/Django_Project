da98d2c7bc897f1a49ecfe7829b0e20034fc897c
Zrazhevskii <vbn-134@yandex.ru>
2023-02-07 17:44:34 +0300

    изменение get_permissions

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -4,7 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
-from advertisements.permissions import IsAdminOrReadOnly
+from advertisements.permissions import AccessPermission
 from advertisements.serializers import AdvertisementSerializer
 
 
@@ -20,13 +20,21 @@ class AdvertisementViewSet(ModelViewSet):
     serializer_class = AdvertisementSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = AdvertisementFilter
-    permission_classes = [IsAdminOrReadOnly, IsAuthenticated]
+    # permission_classes = [IsAdminOrReadOnly, IsAuthenticated]
 
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
 
     def get_permissions(self):
-        """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAdminOrReadOnly()]
-        return []
+        if self.action in ["create"]:
+            permissions = [IsAuthenticated]
+        elif self.action in ["update", "partial_update", "destroy"]:
+            permissions = [AccessPermission]
+        else:
+            permissions = []
+        return [permission() for permission in permissions]
+
+
+
+#Users f9a97466e3cba7008cd32da01d63ee3be3b6de5e
+# Admin 55dc273db6b947e36b983a3987f81333edf3a809
